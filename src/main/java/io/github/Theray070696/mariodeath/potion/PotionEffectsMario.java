11be62001107857f159c47b46dd2ffbc60d02bb6
Theray070696 <Theray070696@gmail.com>
2017-05-02 17:27:43 -0400

    BACK TO THE OLD WAY!

--- src/main/java/io/github/Theray070696/mariodeath/potion/PotionEffectsMario.java
+++ src/main/java/io/github/Theray070696/mariodeath/potion/PotionEffectsMario.java
@@ -2,7 +2,7 @@ package io.github.Theray070696.mariodeath.potion;
 
 import io.github.Theray070696.mariodeath.lib.ModInfo;
 import net.minecraft.potion.Potion;
-import net.minecraftforge.fml.common.registry.GameData;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 /**
  * Created by Theray070696 on 3/25/2017.
@@ -17,10 +17,7 @@ public class PotionEffectsMario
         potionSpiceman.setRegistryName(ModInfo.MOD_ID, "potionSpiceman");
         potionStarman.setRegistryName(ModInfo.MOD_ID, "potionStarman");
 
-        GameData.getPotionRegistry().register(potionSpiceman);
-        GameData.getPotionRegistry().register(potionStarman);
-
-        //GameRegistry.register(potionSpiceman);
-        //GameRegistry.register(potionStarman);
+        GameRegistry.register(potionSpiceman);
+        GameRegistry.register(potionStarman);
     }
 }
