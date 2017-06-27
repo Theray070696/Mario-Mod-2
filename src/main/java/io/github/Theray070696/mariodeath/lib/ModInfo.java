be03515a22e965679fb691a5f148a41137958a78
Theray070696 <Theray070696@gmail.com>
2017-06-27 17:08:33 -0400

    Change modid. I didn't want to do this.

--- src/main/java/io/github/Theray070696/mariodeath/lib/ModInfo.java
+++ src/main/java/io/github/Theray070696/mariodeath/lib/ModInfo.java
@@ -6,7 +6,7 @@ package io.github.Theray070696.mariodeath.lib;
 public class ModInfo
 {
 
-    public static final String MOD_ID = "mariodeath";
+    public static final String MOD_ID = "mario2";
     public static final String CHANNEL = MOD_ID;
     public static final String MOD_NAME = "Mario Mod 2";
     public static final String MOD_VERSION = "version";
