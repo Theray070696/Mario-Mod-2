53f9f762de3ce214182be2a45b40dd42bbbb4ac8
theray070696@gmail.com <kirby765>
2017-08-16 11:47:51 -0400

    Pipes are now metalic

--- src/main/java/io/github/Theray070696/mariodeath/block/BlockPipeBase.java
+++ src/main/java/io/github/Theray070696/mariodeath/block/BlockPipeBase.java
@@ -1,5 +1,7 @@
 package io.github.Theray070696.mariodeath.block;
 
+import net.minecraft.block.material.Material;
+
 /**
  * Created by Theray070696 on 2/9/2017.
  */
@@ -7,7 +9,7 @@ public class BlockPipeBase extends BlockMario
 {
     public BlockPipeBase()
     {
-        super();
+        super(Material.IRON);
         
         this.setUnlocalizedName("marioBlockPipeBase");
     }
