29a705927618300ab603d591ea44f7387ae98536
Theray070696 <Theray070696@gmail.com>
2017-08-17 11:09:51 -0400

    Everything is vine

--- src/main/java/io/github/Theray070696/mariodeath/block/BlockBeanstalk.java
+++ src/main/java/io/github/Theray070696/mariodeath/block/BlockBeanstalk.java
@@ -1,5 +1,6 @@
 package io.github.Theray070696.mariodeath.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
@@ -7,11 +8,15 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.Random;
 
@@ -40,7 +45,7 @@ public class BlockBeanstalk extends BlockMario
 
     public void breakBlock(World world, BlockPos pos, IBlockState blockState)
     {
-        if(world.getBlockState(pos).getBlock() instanceof BlockBeanstalk)
+        if(world.getBlockState(pos.up()).getBlock() instanceof BlockBeanstalk)
         {
             world.destroyBlock(pos.up(), false);
         }
@@ -53,6 +58,27 @@ public class BlockBeanstalk extends BlockMario
             ++stack.stackSize;
             world.destroyBlock(pos, false);
         }
+    }
+
+    @Override
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
+    {
+        if(side.equals(EnumFacing.DOWN))
+        {
+            return false;
+        }
+
+        if(side.equals(EnumFacing.UP) && !blockState.getValue(ISTOP))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isFullCube(IBlockState state)
+    {
+        return false;
     }
 
     public boolean isOpaqueCube(IBlockState state)
@@ -68,6 +94,12 @@ public class BlockBeanstalk extends BlockMario
     public EnumBlockRenderType getRenderType(IBlockState state)
     {
         return EnumBlockRenderType.MODEL;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
     }
 
     public boolean isLadder(IBlockState state, IBlockAccess world, BlockPos pos, EntityLivingBase entity)
@@ -88,6 +120,14 @@ public class BlockBeanstalk extends BlockMario
     @Override
     public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos)
     {
-        return state.withProperty(ISTOP, !(world.getBlockState(pos.up()).getBlock() instanceof BlockBeanstalk));
+        Block block = world.getBlockState(pos.up()).getBlock();
+
+        if(block instanceof BlockBeanstalk)
+        {
+            return state.withProperty(ISTOP, false);
+        } else
+        {
+            return state.withProperty(ISTOP, true);
+        }
     }
 }
