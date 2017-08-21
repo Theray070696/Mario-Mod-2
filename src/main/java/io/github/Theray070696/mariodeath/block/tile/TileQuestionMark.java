23a41d659d3123b27b8a4bc2fa48fec45a6888de
Theray070696 <Theray070696@gmail.com>
2017-08-21 15:29:40 -0400

    Fix bug caused by AE2 meteors

--- src/main/java/io/github/Theray070696/mariodeath/block/tile/TileQuestionMark.java
+++ src/main/java/io/github/Theray070696/mariodeath/block/tile/TileQuestionMark.java
@@ -1,15 +1,30 @@
 package io.github.Theray070696.mariodeath.block.tile;
 
+import io.github.Theray070696.mariodeath.block.BlockQuestionMark;
 import io.github.Theray070696.mariodeath.lib.ItemsInQuestionMarks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
 
 /**
  * Created by Theray070696 on 8/27/2015.
  */
-public class TileQuestionMark extends TileEntity
+public class TileQuestionMark extends TileEntity implements ITickable
 {
     private int itemInBlock = ItemsInQuestionMarks.ITEM_NOTHING;
+    private boolean shouldRun = true;
+
+    public void update()
+    {
+        if(shouldRun)
+        {
+            shouldRun = false;
+            if(this.worldObj.getBlockState(this.pos).getBlock() instanceof BlockQuestionMark && this.itemInBlock == ItemsInQuestionMarks.ITEM_NOTHING)
+            {
+                this.worldObj.setBlockToAir(this.pos);
+            }
+        }
+    }
 
     public void readFromNBT(NBTTagCompound nbtTagCompound)
     {
