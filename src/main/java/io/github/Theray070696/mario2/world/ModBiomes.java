acbfd5acfc5c701bd7bb8ea079321e7e5468f8d6
Theray070696 <Theray070696@gmail.com>
2017-11-10 16:33:36 -0500

    Make biomes rarer

--- src/main/java/io/github/Theray070696/mario2/world/ModBiomes.java
+++ src/main/java/io/github/Theray070696/mario2/world/ModBiomes.java
@@ -20,19 +20,19 @@ public class ModBiomes
     public static void initBiomes()
     {
         biomeMarioPlains = GameRegistry.register(new BiomeMarioPlains());
-        BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(biomeMarioPlains, 5));
+        BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(biomeMarioPlains, 2));
         BiomeManager.addSpawnBiome(biomeMarioPlains);
         BiomeDictionary.registerBiomeType(biomeMarioPlains, BiomeDictionary.Type.PLAINS);
 
         biomeMarioForest = GameRegistry.register(new BiomeMarioForest("biomeMarioForest", new Biome.BiomeProperties("Mario Forest").setBaseBiome
                 ("Forest").setTemperature(0.7F).setRainfall(0.8F)));
-        BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(biomeMarioForest, 5));
+        BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(biomeMarioForest, 2));
         BiomeManager.addSpawnBiome(biomeMarioForest);
         BiomeDictionary.registerBiomeType(biomeMarioForest, BiomeDictionary.Type.FOREST);
 
         biomeMarioForestHills = GameRegistry.register(new BiomeMarioForest("biomeMarioForestHills", new Biome.BiomeProperties("Mario Forest Hills")
                 .setBaseBiome("ForestHills").setBaseHeight(0.45F).setHeightVariation(0.3F).setTemperature(0.7F).setRainfall(0.8F)));
-        BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(biomeMarioForestHills, 5));
+        BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(biomeMarioForestHills, 2));
         BiomeManager.addSpawnBiome(biomeMarioForestHills);
         BiomeDictionary.registerBiomeType(biomeMarioForestHills, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.HILLS);
     }
